ea771563fc55f6f4b84f7f88ef6c6f16b6b4275c
TemirovaAzima <temirovaazima9@gmail.com>
2025-01-29 19:41:32 +0500

    using conditions with useEffect end

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import {useState, useEffect} from 'react'
 
 export const App = ()=>{
-    const [value, setValue] = useState(0);
-    const [smth, setSmth] = useState(1);
+     const [value, setValue] = useState(0);
+     const [smth, setSmth] = useState(1);
 
     useEffect(()=>{
-        console.log('UseEffect hook')
-        document.title = `Increment ${value} and ${smth}`
-    },[value,smth])
+        if(value>3){
+            console.log('UseEffect hook')
+            document.title = `Increment ${value} and ${smth}`
+        }
+    }, [value,smth])
     return(
        <div>
-           <h2>{value}</h2>
-           <h2>{smth}</h2>
-           <button onClick={()=> setValue(value+1)}>Increment</button>
-           <button onClick={()=> setSmth(smth+1)}>Increment2</button>
+           <h1>{value} {smth} </h1>
+           <button onClick={()=>setValue(value+1)}>Increment</button>
+           <button onClick={()=> setSmth(smth+1)}>Increment</button>
        </div>
     )
 }
