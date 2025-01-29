cf075c837da0944e7d913bf141af549b61422541
TemirovaAzima <temirovaazima9@gmail.com>
2025-01-29 19:23:05 +0500

    using two deps end

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import {useState, useEffect} from 'react'
 
 export const App = ()=>{
- const [value, setValue] = useState(0)
+    const [value, setValue] = useState(0);
+    const [smth, setSmth] = useState(1);
 
-    useEffect(() =>{
-        console.log('UseEffect hooki')
-        document.title = `Increment ${value}`
-    },[value]);
+    useEffect(()=>{
+        console.log('UseEffect hook')
+        document.title = `Increment ${value} and ${smth}`
+    },[value,smth])
     return(
-        <div>
-            <h2>{value}</h2>
-            <button onClick={()=>setValue(value+1)}>Increment</button>
-        </div>
+       <div>
+           <h2>{value}</h2>
+           <h2>{smth}</h2>
+           <button onClick={()=> setValue(value+1)}>Increment</button>
+           <button onClick={()=> setSmth(smth+1)}>Increment2</button>
+       </div>
     )
 }
